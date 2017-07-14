5eb3700858a3cbefce3120234de0d99b6b92c3a1
Jake <jake83@gmail.com>
2017-07-13 20:09:05 -0700

    add precommit prettier

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Cell from './Cell'
+import Cell from "./Cell";
 
 class App extends Component {
-    render() {
-        const { boardWidth, board, cells, toggleFlag, open } = this.props
-        return (
-            <div
-                className="App"
-                style={{
-                    display: 'flex',
-                    flexFlow: 'row wrap',
-                    // width: ROWS * 20
-                    width: boardWidth
-                }}
-            >
-                {board.map(id =>
-                    <Cell
-                        key={id}
-                        onRightClick={toggleFlag}
-                        onClick={open}
-                        {...cells[id]}
-                    />
-                )}
-            </div>
-        )
-    }
+  render() {
+    const { boardWidth, board, cells, toggleFlag, open } = this.props;
+    return (
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          flexFlow: "row wrap",
+          width: boardWidth
+        }}
+      >
+        {board.map(id =>
+          <Cell
+            key={id}
+            onRightClick={toggleFlag}
+            onClick={open}
+            {...cells[id]}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const { allIds, byId } = state.board;
-    console.log(state.options)
-    return { board: allIds, cells: byId, boardWidth: state.options.rows * 20 }
-}
+  const { allIds, byId } = state.board;
+  console.log(state.options);
+  return { board: allIds, cells: byId, boardWidth: state.options.rows * 20 };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        toggleFlag: id => {
-            dispatch({ type: 'CELL_TOGGLE_FLAG', id })
-        },
-        open: id => {
-            dispatch({ type: 'CELL_OPEN', id })
-        }
+  return {
+    toggleFlag: id => {
+      dispatch({ type: "CELL_TOGGLE_FLAG", id });
+    },
+    open: id => {
+      dispatch({ type: "CELL_OPEN", id });
     }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
