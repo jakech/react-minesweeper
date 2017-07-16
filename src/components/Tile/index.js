856eb45aa7d6586013796023cfb24eaeca932a49
Jake <jake83@gmail.com>
2017-07-16 02:51:04 -0700

    styled component for tiles

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const flag = '\uD83D\uDEA9'
-const bomb = '\uD83D\uDCA3'
+const FLAG = '\uD83D\uDEA9'
+const BOMB = '\uD83D\uDCA3'
+
+const Button = styled.button`
+    box-sizing: border-box;
+    flex: 1 1 auto;
+    width: 20px;
+    height: 20px;
+    padding: 0;
+    margin: 0;
+    line-height: 20px;
+    text-align: center;
+    border: 1px white solid;
+    background: ${props =>
+        props.highlight ? 'red' : props.isOpen ? 'lightgray' : 'darkgray'};
+`
 
 export default ({
     isOpen,
@@ -14,27 +29,15 @@ export default ({
     onRightClick
 }) => {
     return (
-        <button
-            style={{
-                flex: '1 1 auto',
-                width: 20,
-                height: 20,
-                lineHeight: '20px',
-                background: highlight
-                    ? 'red'
-                    : isOpen ? 'lightgray' : 'darkgray',
-                textAlign: 'center',
-                border: '1px white solid',
-                boxSizing: 'border-box',
-                padding: 0,
-                margin: 0
-            }}
+        <Button
+            highlight={highlight}
+            isOpen={isOpen}
             disabled={disabled}
             onClick={onClick}
             onContextMenu={onRightClick}
         >
-            {isFlagged && flag}
-            {isOpen && (hasMine ? bomb : !!value && value)}
-        </button>
+            {isFlagged && FLAG}
+            {isOpen && (hasMine ? BOMB : !!value && value)}
+        </Button>
     )
 }
